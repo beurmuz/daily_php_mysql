9915f7adc62daa88acb0b20cfa51b6fc0df6c7d7
beurmuz <fallinta2@gmail.com>
2021-02-18 23:16:02 +0900

    number.php

---
+++ htdocs/number.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>number.php</title>
+</head>
+<body>
+    <h1> Number & Arithmetic Operator</h1>
+    <h2> 1+1 </h2>
+    <?php
+        echo 1+1;
+    ?>
+
+    <h2> 2-1 </h2>
+    <?php
+        echo 2-1;
+    ?>
+
+    <h2> 3*4 </h2>
+    <?php
+        echo 3*4;
+    ?>
+
+    <h2> 8/2 </h2>
+    <?php
+        echo 8/2;
+    ?>
+</body>
+</html>
