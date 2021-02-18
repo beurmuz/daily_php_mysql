b789f1c4a8e37b9a96bad2dd69af54a8fcbcf32c
beurmuz <fallinta2@gmail.com>
2021-02-18 23:16:35 +0900

    string.php

---
+++ htdocs/string.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>string.php</title>
+</head>
+<body>
+    <h1>String & String Operator</h1>
+    <?php
+        echo 'Hello Friend';
+    ?>
+
+    <h2>"",'' operator</h2>
+    <?php
+        echo 'Hello "F"riend';
+    ?>
+
+    <h2>'\'operator</h2>
+    <?php
+        echo 'Hello \'F\'riend';
+    ?>
+
+    <h2>concatenation operator</h2>
+    <?php
+        echo 'Hello '.'FFFFriend';
+    ?>
+
+    <h2>String length function</h2>
+    <?php
+        echo strlen("Hello minsik!");
+    ?>
+</body>
+</html>
