43ff5eee1ddd9ff75c1d6ad3be0b25e3a0af79b3
beurmuz <fallinta2@gmail.com>
2021-02-18 23:16:55 +0900

    variables.php

---
+++ htdocs/variables.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>variables.php</title>
+</head>
+<body>
+    <h1>variable</h1>
+    <h2>variable?</h2>
+    <?php   
+        $a = 10;
+        echo $a + 1;
+    ?>
+
+    <h2>use the variable</h2>
+    <?php
+        $var = "rendezvous";
+        echo $var;
+        echo "<p>사실은 생각보다 단순하지 그걸 사랑이라고 난 발음하지 너의 전화기는 오늘도 부재중 Ay girl, why are you so bulletproof
+        약속이 없다면 내게 연락 좀 해줘 난 없던 시간도 너 하나면 생겨 내가 잘아는 영어 단어는 patient  My love is straight up
+        오늘은 너와 나의 ".$var."Just pick the time, 난 좋아 언제든 너를 안을래 이번에는 We are the perfect twoㅡWe are the perfect two
+        I'm the one Who needs your love Just me and you Let's have a ".$var;
+        ?>
+</body>
+</html>
