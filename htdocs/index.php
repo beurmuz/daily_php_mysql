af8998ad0382a12d8716a7e4d00edfe2b2ddeb6b
beurmuz <fallinta2@gmail.com>
2021-02-19 19:13:37 +0900

    isset() 이용하기

--- htdocs/index.php
+++ htdocs/index.php
@@ -5,7 +5,7 @@
         <title></title>
     </head>
 <body>
-    <h1>WEB</h1>
+    <h1><a href="index.php">WEB</a></h1>
     <ol>
         <li><a href="index.php?id=HTML">HTML</a></li>
         <li><a href="index.php?id=CSS">CSS</a></li>
@@ -13,31 +13,20 @@
     </ol>
     <h2>
         <?php 
-            echo $_GET['id']; 
+            if(isset($_GET['id'])) { // 값이 세팅되어 있으면
+                echo $_GET['id'];
+            } else{ // 값이 세팅되어 있지 않으면
+                echo "Welcome";
+            }
         ?>
     </h2>
     <?php
-        echo file_get_contents("data/".$_GET['id']);
+        if(isset($_GET['id'])) {
+            echo file_get_contents("data/".$_GET['id']);
+        }else {
+            echo "Hello. PHP!";
+        }
+        
     ?>
-    cus I was moving way up no ceiling
-    I grabbed a chance heaven sent
-    it ain’t been the same ever since
-    I’m waking up, sweating every morning
-    fighting myself in my dream
-    I’m my own opponent
-    every now and then
-    I pop a pill to numb it down
-    voice inside my head I need to turn it down yea
-    can’t hold it down any longer I’m clipping
-    swear all these people just praying til I’m slipping
-    one thing I can’t ignore is that I’m different
-    the way I envision my future ambitions
-    chips on the table, I raise to the max
-    can’t beat my hand, Imma leave with the bags
-    riding the wave I create on my own
-    Imma jump in and give it all
-    cus Imma do this til I die
-    Imma do this til I die
-    cus imma do this til I die
 </body>
 </html>
