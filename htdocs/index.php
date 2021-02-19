eae723177a4af72eac31f0ae0bcbffde743684a1
beurmuz <fallinta2@gmail.com>
2021-02-19 18:16:49 +0900

    index.php

---
+++ htdocs/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+    </head>
+<body>
+    <h1>WEB</h1>
+    <ol>
+        <li><a href="index.php?id=HTML">HTML</a></li>
+        <li><a href="index.php?id=CSS">CSS</a></li>
+        <li><a href="index.php?id=JavaScript">JavaScript</a></li>
+    </ol>
+    <h2>
+        <?php echo $_GET['id']; ?>
+    </h2>
+    cus I was moving way up no ceiling
+    I grabbed a chance heaven sent
+    it ain’t been the same ever since
+    I’m waking up, sweating every morning
+    fighting myself in my dream
+    I’m my own opponent
+    every now and then
+    I pop a pill to numb it down
+    voice inside my head I need to turn it down yea
+    can’t hold it down any longer I’m clipping
+    swear all these people just praying til I’m slipping
+    one thing I can’t ignore is that I’m different
+    the way I envision my future ambitions
+    chips on the table, I raise to the max
+    can’t beat my hand, Imma leave with the bags
+    riding the wave I create on my own
+    Imma jump in and give it all
+    cus Imma do this til I die
+    Imma do this til I die
+    cus imma do this til I die
+</body>
+</html>
