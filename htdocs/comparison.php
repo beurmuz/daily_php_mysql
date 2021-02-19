a6df9febe5479d7aaa990f5c9b0a601de12a4d88
beurmuz <fallinta2@gmail.com>
2021-02-19 18:59:06 +0900

    conditional statement

---
+++ htdocs/comparison.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>comparison.php</title>
+</head>
+<body>
+    <h1>Comparison Operators & Boolean data type</h1>
+    <h2>var_dump안에 숫자 입력시</h2>
+    <?php
+        var_dump(15);
+    ?>
+
+    <h2>var_dump안에 문자 입력시</h2>
+    <?php
+        var_dump("15");
+    ?>
+
+    <h2>var_dump()로 연산 결과 확인</h2>
+    <?php
+        var_dump(2+1);
+    ?>
+
+    <h2>var_dump()로 1==1 결과 확인</h2>
+    <?php
+        var_dump(1==1);
+    ?>
+
+    <h2>var_dump()로 1>=3 결과 확인</h2>
+    <?php
+        var_dump(1>=3);
+    ?>
+</body>
+</html>
---
+++ htdocs/conditional.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>conditional.php</title>
+</head>
+<body>
+    <h1>Conditional</h1>
+    <?php
+        echo '1<br>';
+        echo '2<br>';
+        echo '3<br>';
+    ?>
+
+    <h2>if</h2>
+    <?php
+        echo '1<br>';
+        if(false){
+            echo '2<br>';
+        }
+        echo '3<br>';
+    ?>
+
+    <h2>if/false</h2>
+    <?php
+        echo '1<br>';
+        if(true){
+            echo '2-1<br>';
+        }else {
+            echo '2-2<br>';
+        }
+        echo '3<br>';
+    ?>
+</body>
+</html>
