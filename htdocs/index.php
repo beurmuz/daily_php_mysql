a3f5ae0bb0f7f67018d62b1a3da0d6e86e86b2fd
beurmuz <fallinta2@gmail.com>
2021-02-21 18:02:53 +0900

    기능을 함수로 만들기

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,8 +1,43 @@
+<?php
+    function print_title(){
+        if(isset($_GET['id'])) {
+            echo $_GET['id'];
+        } else {
+            echo "Welcome";
+        }
+    }
+
+    function print_description() {
+        if(isset($_GET['id'])) {
+            echo file_get_contents("data/".$_GET['id']);
+        } else {
+            echo "Hello, PHP";
+        }
+    }
+
+    function print_list() {
+        $list = scandir('./data');
+        $i = 0;
+        while($i<count($list)){
+            if($list[$i]!='.') {
+                if($list[$i]!='..') {
+                    echo "<li><a href=\"index.php?id=$list[$i]\">$list[$i]</a></li>\n";
+                }
+            }
+            $i = $i +1;
+        }
+    }
+?>
+
 <!DOCTYPE html>
 <html>
     <head>
         <meta charset="utf-8">
-        <title></title>
+        <title>
+            <?php
+                print_title();
+            ?>
+        </title>
     </head>
 <body>
     <h1><a href="index.php">WEB</a></h1>
@@ -11,16 +46,7 @@
         <li><a href="index.php?id=CSS">CSS</a></li>
         <li><a href="index.php?id=JavaScript">JavaScript</a></li> -->
         <?php
-            $list = scandir('./data');
-            $i = 0;
-            while($i<count($list)) {
-                if($list[$i] != '.') {
-                    if($list[$i] != '..') { 
-                        echo "<li><a href=\"index.php?id=$list[$i]\">$list[$i]</a></li>\n";
-                    }
-                }
-                $i = $i + 1;
-            }
+           print_list();
             // echo "<li>$list[0]</li>\n";
             // echo "<li>$list[1]</li>\n";
             // echo "<li>$list[2]</li>\n";
@@ -31,20 +57,11 @@
     </ol>
     <h2>
         <?php 
-            if(isset($_GET['id'])) { // 값이 세팅되어 있으면
-                echo $_GET['id'];
-            } else{ // 값이 세팅되어 있지 않으면
-                echo "Welcome";
-            }
+            print_title();
         ?>
     </h2>
     <?php
-        if(isset($_GET['id'])) {
-            echo file_get_contents("data/".$_GET['id']);
-        }else {
-            echo "Hello. PHP!";
-        }
-        
+        print_description();
     ?>
 </body>
 </html>
