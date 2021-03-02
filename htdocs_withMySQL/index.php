ff350375ae170814c3b86bcebb1e174d6a902a01
beurmuz <fallinta2@gmail.com>
2021-03-02 14:25:56 +0900

    create and insert

---
+++ htdocs_withMySQL/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>WEB</title>
+</head>
+<body>
+    <h1>WEB</h1>
+    <ol>
+        <li>HTML</li>
+    </ol>
+    <a href="create.php">create</a>
+    <h2>Welcome</h2>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+</body>
+</html>
