47ec9e233bc452eb5537bc20437e9b7da64a0265
beurmuz <fallinta2@gmail.com>
2021-03-04 21:47:51 +0900

    PHP데이터 타입으로 전환

--- htdocs_withMySQL/select.php
+++ htdocs_withMySQL/select.php
@@ -1,6 +1,11 @@
 <?php
     $conn = mysqli_connect("localhost", "root", "S10803a", "opentutorials");
-    $sql = "SELECT *FROM topic";
+
+    echo "<h1>single row</h1>"; 
+    $sql = "SELECT *FROM topic WHERE id ='5';
     $result = mysqli_query($conn, $sql);
-    var_dump($result->num_rows);
-?>
+    -- var_dump($result->num_rows);
+    $row = mysqli_fetch_array($result); // 배열로 가져오기
+    echo '<h2>'.$row['title'].'</h2>';
+    echo $row['description'];
+?> 
